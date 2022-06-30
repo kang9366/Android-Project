af3da60eb20430607a87b13907d1b70a95e6fbca
SeungGu Kang <alex9366@naver.com>
2022-07-01 04:15:48 +0900

    Lotto Number Picker

---
+++ LottoNumber/app/src/main/java/com/sgkang/lottonumber/MainFragment.kt
@@ -0,0 +1,12 @@
+package com.sgkang.lottonumber
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+
+class MainFragment: Fragment() {
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+
+}
