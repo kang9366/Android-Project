44d250a99884901331bde86847facd23a3cb2331
SeungGu Kang <alex9366@naver.com>
2023-03-04 10:52:59 +0900

    기본 UI 구성

--- Recorder/app/src/main/java/com/example/recorder/MainActivity.kt
+++ Recorder/app/src/main/java/com/example/recorder/MainActivity.kt
@@ -4,8 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private val recordButton: RecordButton by lazy{
+        findViewById(R.id.recordButton)
+    }
+
+    private var state = State.BEFORE_RECORDEING
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    fun initViews(){
+        recordButton.updateIconWithState(state)
     }
 }
---
+++ Recorder/app/src/main/java/com/example/recorder/State.kt
@@ -0,0 +1,8 @@
+package com.example.recorder
+
+enum class State {
+    BEFORE_RECORDEING,
+    ON_RECORDING,
+    AFTER_RECORDING,
+    ON_PLAYING
+}
