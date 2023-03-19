de31cb476a07133c7ea7ddcf1504311b43b6aa05
SeungGu Kang <alex9366@naver.com>
2023-03-19 10:38:08 +0900

    UI구성 및 FloatingActionButton Test

--- EnglishStudy/app/src/main/java/com/example/englishstudy/MainActivity.kt
+++ EnglishStudy/app/src/main/java/com/example/englishstudy/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.englishstudy
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.englishstudy.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val floatingButton: FloatingActionButton = binding.floatingButton
+        floatingButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+                .setTitle("Test")
+                .setMessage("Test")
+                .setPositiveButton("확인",
+                DialogInterface.OnClickListener{ dialog, which ->
+                    Toast.makeText(this, "test", Toast.LENGTH_SHORT).show()
+                })
+            builder.show()
+        }
     }
 }
